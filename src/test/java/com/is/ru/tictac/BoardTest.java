cce5a46ca21a3d321fcb3ad93f600bf5b5cd648b
saivald02 <saivald02@gmail.com>
2016-10-25 19:57:57 +0000

    fixed BoardTest class path

---
+++ src/test/java/com/is/ru/tictac/BoardTest.java
@@ -0,0 +1,29 @@
+package com.is.ru.tictac;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+
+public class BoardTest{
+
+    @Test
+    public void testBoardConstructor(){
+        assertNotNull("Should not be null!", new Board());
+    }
+
+    @Test
+    public void testEmptyCell(){
+        Board board = new Board();
+        assertEquals(true, board.isCellEmpty(0,0));
+        assertEquals(true, board.isCellEmpty(1,2));
+    }
+
+    @Test
+    public void testMarkCellPlayed(){
+        Board board = new Board();
+        board.markCellPlayed(1, 0, 0);
+        assertEquals(false, board.isCellEmpty(0,0));
+    }
+
+}
+
