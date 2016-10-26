b75846d8e58fe8f6f166e0b75a6d7ee2c8435484
saivald02 <saivald02@gmail.com>
2016-10-26 01:09:39 +0000

    added function for HumanPlayer to move/play

--- src/main/java/com/is/ru/tictac/GamePlay.java
+++ src/main/java/com/is/ru/tictac/GamePlay.java
@@ -1,12 +1,58 @@
 package com.is.ru.tictac;
 
 public class GamePlay {
-
+    
+    Board board;
+    
     public GamePlay()
     {
-
+	board = new Board(); 
     }
     
+    public void playerMove(int player, int humanOrComputer) 
+    {  
+	if(humanOrComputer == 0) // if player is human == 0
+	{
+	    humanMove(player); 
+	}
+	else	// if player is computer == 1
+	{
+	    computerMove(player);
+	}
+    }
+
+    private void humanMove(int player)
+    {
+	// get human input
+	int x = 0;
+	int y = 0;
+	while(true)
+	{
+	    if(isMoveLegal(x, y) && board.isCellEmpty(x, y))
+	    {
+		playedMove(player, x, y);
+		break;
+	    }
+	    else
+	    {
+		// tell player that inputIsTakenOrIllegal
+		// ask the player to give input again
+		x = 1;
+	        y = 1;
+	    }	
+	}
+    }
+    
+    
+    public void computerMove(int player)
+    {
+	// computer move
+    }
+    
+    public void playedMove(int player, int row, int col) // remember to change to private after tests
+    {
+	board.markCellPlayed(player, row, col);
+    }
     
     public boolean isMoveLegal(int x, int y) // remember to change to private after tests
     {
