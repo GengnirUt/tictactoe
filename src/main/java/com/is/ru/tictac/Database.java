548814b098038c865c9406099fa362a9ad03423d
Jón Örn Brynjarsson <jonob06@ru.is>
2016-10-27 10:43:03 +0000

    Added a function to select how often a player has won in a row now, plus a function to see all tuples in the gameinfo relation

--- src/main/java/com/is/ru/tictac/Database.java
+++ src/main/java/com/is/ru/tictac/Database.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.sql.ResultSet;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.io.File;
@@ -21,6 +22,7 @@ public class Database{
 	+ "gid INTEGER PRIMARY KEY,\n"
 	+ "winnerid INTEGER NOT NULL, \n"
 	+ "moves INTEGER, \n"
+	+ "datetime REAL, \n"
 	+ "FOREIGN KEY (winnerid) REFERENCES playerinfo(pid)\n"
 	+ ");";
 
@@ -31,12 +33,17 @@ public class Database{
 
 
     public static void insertNewGame(int winner, int moves){
-	String sql = "INSERT INTO gameinfo(winnerid, moves) VALUES (?,?)";
+	String sqlDate = "SELECT julianday('now') as date";
+	String sql = "INSERT INTO gameinfo(winnerid, moves, datetime) VALUES (?,?,?)";
 
 	try (Connection conn = databaseConnect();
+	     PreparedStatement pstmtDate = conn.prepareStatement(sqlDate);
 	     PreparedStatement pstmt = conn.prepareStatement(sql)) {
+		Double date = Double.parseDouble(pstmtDate.executeQuery().getString("date"));
+
 		pstmt.setInt(1, winner);
 		pstmt.setInt(2, moves);
+		pstmt.setDouble(3, date);
 		pstmt.executeUpdate();
 
 	    } catch (SQLException e) {
@@ -115,6 +122,7 @@ public class Database{
 	insertIntoPlayerinfo();
     }
 
+
     private static void insertIntoPlayerinfo(){
 	String p1sql = "INSERT INTO playerinfo(pid, name) VALUES (1, \"Humanoid 1\")";
 	String p2sql = "INSERT INTO playerinfo(pid, name) VALUES (1, \"Humanoid 2\")";
@@ -135,16 +143,52 @@ public class Database{
     }
 
     public int selectPlayerHasWon(int playerID){
-	String sql = "SELECT count(gid) AS wins FROM gameinfo WHERE gid = ?";
+	String sql = "SELECT count(gid) AS wins FROM gameinfo WHERE winnerID = ?";
 
 	try (Connection conn = databaseConnect();
              PreparedStatement pstmt  = conn.prepareStatement(sql)){
-            	pstmt.setDouble(1, playerID);	
+            	pstmt.setInt(1, playerID);	
 		return pstmt.executeQuery().getInt("wins");
             
 	    } catch (SQLException e) {
             System.out.println(e.getMessage());
 	    return -1;
+        }
+    }
+
+
+
+    public int selectPlayerWinningStreak(int playerID){
+        String sql = "SELECT count(gid) AS streak FROM gameinfo WHERE winnerID = ? AND \n"
+	    + "datetime > (SELECT MAX(datetime) FROM gameinfo WHERE winnerID <> ?)";
+
+        try (Connection conn = databaseConnect();
+             PreparedStatement pstmt  = conn.prepareStatement(sql)){
+                pstmt.setInt(1, playerID);
+		pstmt.setInt(2, playerID);
+                return pstmt.executeQuery().getInt("streak");
+
+            } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return -1;
+        }
+    }
+    public void selectAll(){
+        String sql = "SELECT * FROM gameinfo";
+
+        try (Connection conn = databaseConnect();
+             PreparedStatement pstmt  = conn.prepareStatement(sql)){
+             ResultSet rs = pstmt.executeQuery();
+	     
+	     while(rs.next()){
+		 System.out.println("Game: " + rs.getInt("gid") + "\t" +
+				    "Player: " + rs.getInt("winnerID") + "\t" +
+				    "Moves: " + rs.getInt("moves") + "\t" +
+				    "Time: " + rs.getString("datetime"));
+	     }
+
+            } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
 
