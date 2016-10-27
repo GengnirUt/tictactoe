2fdf81a20b222f298c0695906f8505945bb30784
Jón Örn Brynjarsson <jonob06@ru.is>
2016-10-27 07:16:54 +0000

    Moved directory check into a private function

--- src/main/java/com/is/ru/tictac/Database.java
+++ src/main/java/com/is/ru/tictac/Database.java
@@ -5,23 +5,23 @@ import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.io.File;
+import java.io.IOException;
 
 
 // Code from sqlitetutorial.net
 
 public class Database{
 
+    private static final String dirName = "./Data/";
+    private static final String fileName = "GameInfo.db";
 
-    public static void createNewDatabase(String fileName){
-	String dirName = "./Data/";
+    public static void createNewDatabase(){
 	File dir = new File(dirName);
 
 	String url = "jdbc:sqlite:" + dirName + fileName;
 
-	if(!dir.exists()){
-	    dir.mkdir();
-	}
-	
+	checkDbDirectory(dir);
+
 	try(Connection conn = DriverManager.getConnection(url)){
 		if (conn != null){
 		    DatabaseMetaData meta = conn.getMetaData();
@@ -33,5 +33,42 @@ public class Database{
                 System.out.println(e.getMessage());
         }
     }
+
+
+    private static void checkDbDirectory(File directory){
+	//	try{
+	    if(!directory.exists())
+	    	directory.mkdir();
+
+	    //	} catch(IOException e){
+
+	    //	    System.out.println(e.getMessage());
+	    //	}
+    }
+
+
+    public static void databaseConnect() {
+        Connection conn = null;
+
+        try {
+            String url = "jdbc:sqlite:"+dirName + fileName;
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("Connection to SQLite has been established.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
 
 }
